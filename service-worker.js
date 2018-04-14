31cdbf1d86162aae3ac275d36549cc372a7bcffc
avelar <fortunato.avelar@gmail.com>
2018-04-14 17:54:44 -0300

    improve icon and WebApp colors

--- service-worker.js
+++ service-worker.js
@@ -7,20 +7,17 @@ let files = [
 ]
 
 self.addEventListener("install", function(){
-    console.log("Instalou")
-    caches.open("week-number-files").then(cache => {
-        cache.addAll(files)
-    })
+  console.log("week number ready")
+  caches.open("week-number-files").then(cache => {
+    cache.addAll(files)
+  })
 })
 
 self.addEventListener("fetch", function(event){
-
     let pedido = event.request
     let promiseResposta = caches.match(pedido).then(respostaCache => {
         let resposta = respostaCache ? respostaCache : fetch(pedido)
         return resposta
     })
-
     event.respondWith(promiseResposta)
-
 })
