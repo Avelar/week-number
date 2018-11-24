d4688901d06a72e46a1ff7f5b713b6103c7b7416
avelar <fortunato.avelar@gmail.com>
2018-11-24 16:06:31 -0200

    added index.html on manifet

---
+++ _site/service-worker.js
@@ -0,0 +1,32 @@
+const version = 2;
+
+let files = [
+"/",
+"css/application.css",
+"js/lib/jquery.min.js",
+"js/application.js",
+"img/icon.png",
+"https://fonts.googleapis.com/css?family=Montserrat:400,900",
+]
+
+self.addEventListener("install", function(){
+  console.log("week number ready");
+
+  caches.open("week-number-files-" + version).then(cache => {
+    cache.addAll(files)
+      .then(function(){
+        caches.delete("week-number-files-" + (version - 1));
+        caches.delete("week-number-files");
+      });
+  });
+
+});
+
+self.addEventListener("fetch", function(event){
+    let pedido = event.request
+    let promiseResposta = caches.match(pedido).then(respostaCache => {
+        let resposta = respostaCache ? respostaCache : fetch(pedido)
+        return resposta
+    })
+    event.respondWith(promiseResposta)
+})
